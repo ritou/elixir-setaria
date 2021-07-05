fd2eace9db064c2fd12b9c1f7fd3ffa985b69f2a
Ryo Ito <ritou.06@gmail.com>
2021-07-05 18:35:55 +0900

    version 0 3 (#8)
    * fix deprecated time unit: :seconds warning
    
    * fix application config for :pot
    
    * add github action
    
    * fix validation logic
    
    * use matrix versions
    
    * add latest versions
    
    * Revert "add latest versions"
    
    This reverts commit 06e9c0a827d8fa643260b53a838463b867968051.
    
    * update 22.2 to 22.3
    
    * add 23.3
    
    * add OTP 24.0
    
    * add elixir 1.10.4
    
    * add elixir versions
    
    * set 3 * 3 matrix
    
    * version 0.3.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Setaria.Mixfile do
 
   def project do
     [app: :setaria,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.4",
      # build_embedded: Mix.env == :prod,
      # start_permanent: Mix.env == :prod,
@@ -20,7 +20,7 @@ defmodule Setaria.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :pot]]
   end
 
   # Dependencies can be Hex packages:
@@ -34,7 +34,7 @@ defmodule Setaria.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:pot, "~>0.9.5"},
+      {:pot, "~> 1.0"},
       {:ex_doc, "~> 0.10", only: :dev}
     ]
   end

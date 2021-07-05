0b62e9fbf46b23753761e6efefc0552108b2f682
ritou <ritou.06@gmail.com>
2021-07-05 19:03:46 +0900

    update version to 1.0.0 and fix function name

--- test/setaria_test.exs
+++ test/setaria_test.exs
@@ -12,25 +12,27 @@ defmodule SetariaTest do
     unexpected_token = "263421"
 
     assert Setaria.hotp(encoded_secret, counter) == expected_token
-    assert Setaria.hotp(secret, counter, [encoded_secret: false]) == expected_token
+    assert Setaria.hotp(secret, counter, encoded_secret: false) == expected_token
 
     # valid_hotp
-    assert Setaria.valid_hotp(expected_token, encoded_secret, counter) == true
-    assert Setaria.valid_hotp(expected_token, secret, counter, encoded_secret: false) == true
+    assert Setaria.valid_hotp?(expected_token, encoded_secret, counter) == true
+    assert Setaria.valid_hotp?(expected_token, secret, counter, encoded_secret: false) == true
 
     ## counter
-    assert Setaria.valid_hotp(expected_token, encoded_secret, counter + 1) == false
+    assert Setaria.valid_hotp?(expected_token, encoded_secret, counter + 1) == false
 
     ## token
-    assert Setaria.valid_hotp(unexpected_token, encoded_secret, counter) == false
+    assert Setaria.valid_hotp?(unexpected_token, encoded_secret, counter) == false
 
-    ##secret
-    assert Setaria.valid_hotp(expected_token, encoded_invalid_secret, counter) == false
-    assert Setaria.valid_hotp(expected_token, invalid_secret, counter, encoded_secret: false) == false
+    ## secret
+    assert Setaria.valid_hotp?(expected_token, encoded_invalid_secret, counter) == false
+
+    assert Setaria.valid_hotp?(expected_token, invalid_secret, counter, encoded_secret: false) ==
+             false
   end
 
   test "totp" do
-    timestamp = 1332083784
+    timestamp = 1_332_083_784
     secret = "1234567890"
     encoded_secret = Base.encode32(secret, padding: false)
     invalid_secret = "1234567891"
@@ -40,31 +42,46 @@ defmodule SetariaTest do
 
     # current timestamp
     assert Setaria.totp(encoded_secret) != expected_token
-    assert Setaria.totp(secret, [encoded_secret: false]) != expected_token
-    assert Setaria.totp(encoded_secret) == Setaria.totp(secret, [encoded_secret: false])
+    assert Setaria.totp(secret, encoded_secret: false) != expected_token
+    assert Setaria.totp(encoded_secret) == Setaria.totp(secret, encoded_secret: false)
 
-    assert Setaria.valid_totp(expected_token, encoded_secret) == false
-    assert Setaria.valid_totp(expected_token, secret, [encoded_secret: false]) == false
-   
+    assert Setaria.valid_totp?(expected_token, encoded_secret) == false
+    assert Setaria.valid_totp?(expected_token, secret, encoded_secret: false) == false
+
     # create 
-    assert Setaria.totp(encoded_secret, [timestamp: timestamp]) == expected_token
-    assert Setaria.totp(secret, [timestamp: timestamp, encoded_secret: false]) == expected_token
+    assert Setaria.totp(encoded_secret, timestamp: timestamp) == expected_token
+    assert Setaria.totp(secret, timestamp: timestamp, encoded_secret: false) == expected_token
 
     # valide succes
-    assert Setaria.valid_totp(expected_token, encoded_secret, [timestamp: timestamp]) == true
-    assert Setaria.valid_totp(expected_token, secret, [encoded_secret: false, timestamp: timestamp]) == true
+    assert Setaria.valid_totp?(expected_token, encoded_secret, timestamp: timestamp) == true
+
+    assert Setaria.valid_totp?(expected_token, secret, encoded_secret: false, timestamp: timestamp) ==
+             true
 
     # validate error
     ## token
-    assert Setaria.valid_totp(unexpected_token, encoded_secret, [timestamp: timestamp]) == false
-    assert Setaria.valid_totp(unexpected_token, secret, [timestamp: timestamp, encoded_secret: false]) == false
+    assert Setaria.valid_totp?(unexpected_token, encoded_secret, timestamp: timestamp) == false
+
+    assert Setaria.valid_totp?(unexpected_token, secret,
+             timestamp: timestamp,
+             encoded_secret: false
+           ) == false
 
     ## secret
-    assert Setaria.valid_totp(expected_token, encoded_invalid_secret, [timestamp: timestamp]) == false
-    assert Setaria.valid_totp(expected_token, invalid_secret, [timestamp: timestamp, encoded_secret: false]) == false
+    assert Setaria.valid_totp?(expected_token, encoded_invalid_secret, timestamp: timestamp) ==
+             false
+
+    assert Setaria.valid_totp?(expected_token, invalid_secret,
+             timestamp: timestamp,
+             encoded_secret: false
+           ) == false
 
     ## timestamp
-    assert Setaria.valid_totp(expected_token, encoded_secret, [timestamp: timestamp + 30]) == false
-    assert Setaria.valid_totp(expected_token, secret, [timestamp: timestamp + 30, encoded_secret: false]) == false
+    assert Setaria.valid_totp?(expected_token, encoded_secret, timestamp: timestamp + 30) == false
+
+    assert Setaria.valid_totp?(expected_token, secret,
+             timestamp: timestamp + 30,
+             encoded_secret: false
+           ) == false
   end
 end
